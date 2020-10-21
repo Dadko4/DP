1680db6625b18a5de67539bc6dba84d75285b5e2
David Masaryk <dadomasaryk@gmail.com>
2020-10-21 18:58:49 +0200

    parametrized model

--- config.py
+++ config.py
@@ -9,8 +9,11 @@ data_generator_config = {
 }
 load_from_file = True
 model_config = {
+ "num_of_inner_conv": 4,
  "timesteps": 256,
- "input_dim": 1
+ "input_dim": 1,
+ "first_and_last": 64,
+ "inner": 32
 }
 seq_path = (r'C:\Users\dadom\Desktop\pripDP\DP\FAST5\nanopore\MAP_Data'
             r'\08_07_16_R9_pUC_BC\MA\downloads\pass\NB07\*.fast5')
--- model.py
+++ model.py
@@ -11,27 +11,23 @@ tf.config.experimental.set_memory_growth(gpus[0], True)
 np.random.seed(0)
 
 
-def model_builder(timesteps=300, input_dim=1):
+def model_builder(timesteps=300, inner=32, firs_and_last=64, input_dim=1,
+                  num_of_inner_conv=3, filter_size=12):
     # initializer = RandomNormal(mean=0.0, stddev=0.05, seed=1)
     input_ = keras.Input(shape=(timesteps, input_dim))
-    x = layers.Conv1D(64, 12, activation='relu', padding='same')(input_)
-    x = layers.MaxPooling1D(2, padding='same')(x)
-    x = layers.Conv1D(32, 12, activation='relu', padding='same')(x)
-    x = layers.MaxPooling1D(2, padding='same')(x)
-    x = layers.Conv1D(32, 12, activation='relu', padding='same')(x)
-    x = layers.MaxPooling1D(2, padding='same')(x)
-    x = layers.Conv1D(32, 12, activation='relu', padding='same')(x)
+    x = layers.Conv1D(firs_and_last, filter_size, 
+                      activation='relu', padding='same')(input_)
+    for _ in num_of_inner_conv:
+        x = layers.MaxPooling1D(2, padding='same')(x)
+        x = layers.Conv1D(inner, filter_size, activation='relu', padding='same')(x)
     encoded = layers.MaxPooling1D(2, padding='same')(x)
 
-    x = layers.Conv1D(32, 12, activation='relu', padding='same')(encoded)
+    x = layers.Conv1D(inner, filter_size, activation='relu', padding='same')(encoded)
     x = layers.UpSampling1D(2)(x)
-    x = layers.Conv1D(32, 12, activation='relu', padding='same')(x)
-    x = layers.UpSampling1D(2)(x)
-    x = layers.Conv1D(32, 12, activation='relu', padding='same')(x)
-    x = layers.UpSampling1D(2)(x)
-    x = layers.Conv1D(64, 12, activation='relu', padding='same')(x)
-    x = layers.UpSampling1D(2)(x)
-    decoded = layers.Conv1D(1, 12, activation='linear', padding='same')(x)
+    for _ in num_of_inner_conv:
+        x = layers.Conv1D(inner, filter_size, activation='relu', padding='same')(x)
+        x = layers.UpSampling1D(2)(x)
+    decoded = layers.Conv1D(1, filter_size, activation='linear', padding='same')(x)
     autoencoder = keras.Model(input_, decoded)
     autoencoder.compile(optimizer='adam', loss='mean_absolute_error')
     return autoencoder
